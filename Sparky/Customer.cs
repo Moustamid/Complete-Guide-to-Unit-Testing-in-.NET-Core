7b702e722a3bda65b5efab88f0b615f17b898e2b
Moustamid <Karim1992moustamid@gmail.com>
2022-04-10 15:45:16 -0500

    Asserting String Values and helper methods

---
+++ Sparky/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace Sparky;
+
+public class Customer
+{
+    
+    public string GreetMessage { get; set; }
+    public string GreetAndCombineNames(string firsName , string lastName)
+    {  
+
+        return GreetMessage =  $"Hello , {firsName} {lastName}";
+    }
+
+}
---
+++ SparkyNUnitTest/CustomerNUnitTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace Sparky;
+
+[TestFixture]
+public class CustomerNUnitTests
+{
+    [Test]
+    public void GreetAndCombineNames_InputFirsNameAndLastName_ReturnFullName()
+    {
+        //- Arrange
+        var customer = new Customer();
+        
+        //- Act
+        var fullName = customer.GreetAndCombineNames("Karim" , "Moustamid" );
+        
+        //-Assert
+        Assert.AreEqual(fullName ,"Hello , Karim Moustamid");
+        Assert.That(customer.GreetMessage , Is.EqualTo("Hello , Karim Moustamid"));
+        Assert.That(customer.GreetMessage , Does.Contain(",")); // Note : Contain is case sensitive 
+        Assert.That(customer.GreetMessage , Does.Contain("moustamid").IgnoreCase); 
+        Assert.That(customer.GreetMessage , Does.StartWith("Hello"));
+        Assert.That(customer.GreetMessage , Does.EndWith("Moustamid"));
+        Assert.That(customer.GreetMessage , Does.Match("Hello , [A-Z{1}][a-z]+ [A-Z{1}][a-z]+"));
+    }
+    
+    
+    
+}
