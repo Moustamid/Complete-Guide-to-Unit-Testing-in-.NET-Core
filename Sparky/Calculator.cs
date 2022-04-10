411acb042d580600ba3541201417ccf40610caa8
Moustamid <Karim1992moustamid@gmail.com>
2022-04-10 12:37:00 -0500

    Initial Commit

---
+++ Sparky/Calculator.cs
@@ -0,0 +1,19 @@
+﻿namespace Sparky;
+
+public class Calculator
+{
+    public int AddNumbers(int a, int b)
+    {
+        return a + b;
+    }
+
+    public bool IsOddNumber(int a)
+    {
+        return a % 2 != 0;
+    }
+    
+    public double AddNumbersDouble(double a, double b)
+    {
+        return a + b;
+    }
+}
