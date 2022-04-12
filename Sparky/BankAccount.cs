4b558ac2bea8516c70c021d37114654f03d87308
Moustamid <Karim1992moustamid@gmail.com>
2022-04-12 00:36:37 -0500

    Setting up the Classes

---
+++ Sparky/BankAccount.cs
@@ -0,0 +1,37 @@
+﻿namespace Sparky;
+
+public class BankAccount
+{
+    public int balance { get; set; }
+    
+    private readonly ILogBook _logBook;
+    
+    public BankAccount(ILogBook logBook)
+    {
+        _logBook= logBook;
+        balance = 0;
+    }
+
+    public bool Deposit(int amount)
+    {
+        _logBook.Message("Deposit invoked");
+        balance += amount;
+        return true;
+    }
+
+    public bool Withrdraw(int amount)
+    {
+        if (amount <= balance)
+        {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    public int GetBalance()
+    {
+        return balance;
+    }
+    
+}
---
+++ SparkyNUnitTest/BankAccountNUnitTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Sparky;
+
+[TestFixture]
+public class BankAccountNUnitTests
+{
+
+    private BankAccount bankAccount;
+    [SetUp]
+    public void Setup()
+    {
+        bankAccount = new BankAccount(new LogFakker());
+    }
+
+    [Test]
+    public void BankDeposit_Add100_ReturnTrue()
+    {
+        var result =bankAccount.Deposit(100);
+        Assert.That(result, Is.True);
+        Assert.That(bankAccount.balance, Is.EqualTo(100));
+    }
+
+}
