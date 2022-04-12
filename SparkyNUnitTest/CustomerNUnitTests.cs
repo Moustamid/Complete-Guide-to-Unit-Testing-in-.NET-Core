5493022f1711c4ce76d66810143d75b4d2e15a10
Moustamid <Karim1992moustamid@gmail.com>
2022-04-11 22:37:48 -0500

    Multiple Assert

--- SparkyNUnitTest/CustomerNUnitTests.cs
+++ SparkyNUnitTest/CustomerNUnitTests.cs
@@ -22,13 +22,17 @@ public class CustomerNUnitTests
         var fullName = customer.GreetAndCombineNames("Karim" , "Moustamid" );
         
         //-Assert
-        Assert.AreEqual(fullName ,"Hello , Karim Moustamid");
-        Assert.That(customer.GreetMessage , Is.EqualTo("Hello , Karim Moustamid"));
-        Assert.That(customer.GreetMessage , Does.Contain(",")); // Note : Contain is case sensitive 
-        Assert.That(customer.GreetMessage , Does.Contain("moustamid").IgnoreCase); 
-        Assert.That(customer.GreetMessage , Does.StartWith("Hello"));
-        Assert.That(customer.GreetMessage , Does.EndWith("Moustamid"));
-        Assert.That(customer.GreetMessage , Does.Match("Hello , [A-Z{1}][a-z]+ [A-Z{1}][a-z]+"));
+        Assert.Multiple(() =>
+        {
+            Assert.AreEqual(fullName ,"Hello , Karim Moustamid");
+            Assert.That(customer.GreetMessage , Is.EqualTo("Hello , Karim Moustamid"));
+            Assert.That(customer.GreetMessage , Does.Contain(",")); // Note : Contain is case sensitive 
+            Assert.That(customer.GreetMessage , Does.Contain("moustamid").IgnoreCase); 
+            Assert.That(customer.GreetMessage , Does.StartWith("Hello"));
+            Assert.That(customer.GreetMessage , Does.EndWith("Moustamid"));
+            Assert.That(customer.GreetMessage , Does.Match("Hello , [A-Z{1}][a-z]+ [A-Z{1}][a-z]+"));
+        });
+
     }
 
     [Test]
