2d8424a5e8b5295ef740c781066b3f4040c667bb
Moustamid <Karim1992moustamid@gmail.com>
2022-04-12 06:06:39 -0500

    Setup with Conditional Return + setup default return value

--- SparkyNUnitTest/BankAccountNUnitTests.cs
+++ SparkyNUnitTest/BankAccountNUnitTests.cs
@@ -28,4 +28,37 @@ public class BankAccountNUnitTests
         Assert.That(bankAccount.balance, Is.EqualTo(100));
     }
 
+    [Test]
+    [TestCase(200,100)]
+    [TestCase(200, 150)]
+    public void BankWithdraw_Withdraw100With200Balance_ReturnsTrue(int balance, int withdraw)
+    {
+        var logMock = new Mock<ILogBook>();
+        logMock.Setup( u => u.LogToDb(It.IsAny<string>())).Returns(true);
+        logMock.Setup( u => u.LogBalanceAfterWithdrawal(It.Is<int>(x => x > 0))).Returns(true);
+        
+        var bankAccount = new BankAccount(logMock.Object);
+        bankAccount.Deposit(balance);
+
+        var result = bankAccount.Withrdraw(withdraw);
+        Assert.IsTrue(result);
+
+    }
+    
+    [Test]
+    [TestCase(200, 300)]
+    public void BankWithdraw_Withdraw300With200Balance_ReturnsFalse(int balance, int withdraw)
+    {
+        var logMock = new Mock<ILogBook>();
+
+        logMock.Setup(u => u.LogBalanceAfterWithdrawal(It.Is<int>(x => x > 0))).Returns(true);
+        //logMock.Setup(u => u.LogBalanceAfterWithdrawal(It.Is<int>(x => x < 0))).Returns(false);
+        logMock.Setup(u => u.LogBalanceAfterWithdrawal(It.IsInRange<int>(int.MinValue,-1,Moq.Range.Inclusive))).Returns(false);
+        BankAccount bankAccount = new(logMock.Object);
+        bankAccount.Deposit(balance);
+        var result = bankAccount.Withrdraw(withdraw);
+        Assert.IsFalse(result);
+    }
+
+
 }
